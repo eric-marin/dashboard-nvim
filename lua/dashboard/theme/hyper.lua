9bce88471e0c5be76ab6e7b85cb9f9b0047f9614
Eric Marin <144999446+eric-marin@users.noreply.github.com>
2024-10-01 14:55:37 +0200

    Update hyper.lua 3

--- lua/dashboard/theme/hyper.lua
+++ lua/dashboard/theme/hyper.lua
@@ -478,7 +478,7 @@ local function project_delete()
         if vim.tbl_count(list) < count then
           return
         end
-        table.remove(list, count)
+        table.remove(list, count+1)
         -- list = vim.list_slice(list, count + 1)
         local str = 'return ' .. vim.inspect(list)
         local handle = io.open(path, 'w+')
